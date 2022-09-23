f525b9845ec48969d9d2dcdadc91e515c0212524
MarkDat <mark.dat.code@gmail.com>
2022-09-23 15:22:02 +0700

    Init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/LunchMenuEnlab'),
+      dir: require('path').join(__dirname, './coverage/MotelManager'),
       subdir: '.',
       reporters: [
         { type: 'html' },
